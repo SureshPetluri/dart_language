85d14feb6f6156f38742088c06f0f885170f4150
sureshpetluri <Sureshpetluri94@gmail.com>
2023-07-22 12:25:42 +0530

    variables

---
+++ bin/basics/variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  // const
+  // final
+  // var
+  // dynamic
+  // String
+  // int
+  // double
+  // bool
+  // List
+  // Map
+
+// difference b/w final and const
+
+// The const keyword is a compile-time constant, whereas the final keyword is a run-time constant.
+  // final The final variable can be set its value at once.
+  // const  Constant variables can't be assigned a value.
+
+// difference b/w var and dynamic
+
+  // var: can't change TYPE of the variable, but can change the VALUE of the variable later in code.
+  // dynamic: can change TYPE of the variable, & can change VALUE of the variable later in code.
+}
