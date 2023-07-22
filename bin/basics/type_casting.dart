f968aba955f0cb0df58b7764d64ede2f44052a36
sureshpetluri <Sureshpetluri94@gmail.com>
2023-07-22 18:04:10 +0530

    type_casting

---
+++ bin/basics/type_casting.dart
@@ -0,0 +1,8 @@
+void main() {
+  bool x = false;
+  String y = x.toString();
+
+  int num = 10;
+  double num1 = num.toDouble();
+  print(x.runtimeType);
+}
--- bin/basics/variables.dart
+++ bin/basics/variables.dart
@@ -16,8 +16,18 @@ void main() {
   // final The final variable can be set its value at once.
   // const  Constant variables can't be assigned a value.
 
+  const bool xIsBool = false;
+  // xIsBool = true;
+  final bool xIsFinal = false;
+  // xIsFinal = true;
+
 // difference b/w var and dynamic
 
   // var: can't change TYPE of the variable, but can change the VALUE of the variable later in code.
   // dynamic: can change TYPE of the variable, & can change VALUE of the variable later in code.
+  var xVar = "qwe";
+  // xVar = 10;
+
+  dynamic xDynamic = "ewfug";
+  xDynamic = 10;
 }
