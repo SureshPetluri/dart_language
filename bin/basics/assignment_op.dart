0bc330fd08a55201fa4daa09586d62625648d292
sureshpetluri <Sureshpetluri94@gmail.com>
2023-07-23 10:31:13 +0530

    if statement

--- bin/basics/assignment_op.dart
+++ bin/basics/assignment_op.dart
@@ -6,4 +6,8 @@ void main() {
   x--;
   print('x = $x');
   x -= 2; //or  x=x-2
+
+  x = x * 2; //or  x *= 2;
+  x = x % 2; // or  x %=2;  Returns the remainder
+  x = x ~/ 2; // or   x ~/= 2;
 }
---
+++ bin/basics/if_statement.dart
@@ -0,0 +1,10 @@
+void main() {
+  var x = 0;
+  if (x > 0) {
+    print("X is Positive");
+  } else if (x < 0) {
+    print("X is Negative");
+  } else {
+    print('x is zero');
+  }
+}
